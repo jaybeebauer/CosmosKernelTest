910a41d156df666e2add5fd2748dfa9538e5f9dd
jaybeebauer <joshybee@gmail.com>
2017-07-30 18:58:55 +1000

    -Initial

--- CosmosKernelTest/Kernel.cs
+++ CosmosKernelTest/Kernel.cs
@@ -9,15 +9,51 @@ namespace CosmosKernelTest
     {
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.WriteLine("Cosmos booted successfully. v0.1.");
         }
         
         protected override void Run()
         {
-            Console.Write("Input: ");
+            Console.Write(@"input:\> ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+               var app = input.Substring(0, input.IndexOf(" "));
+               switch (app)
+               {
+                    case "help":
+                         ShowUsage();
+                         break;
+                    case "?":
+                         ShowUsage();
+                         break;
+                    case "sum":
+                         Sum(input);
+                         break;
+                    default:
+                         Console.Write("Command doesn't exist");
+                         Console.WriteLine(Environment.NewLine);
+                         break;
+               }
         }
-    }
+
+          private void ShowUsage()
+          {
+               Console.WriteLine(Environment.NewLine);
+               Console.WriteLine("Cosmos OS v0.0.1");
+               Console.WriteLine("Writer: Josh Bauer");
+               Console.WriteLine("http://www.darlingridge.com.au");
+          }
+
+          private void Sum(string input)
+          {
+              // int i = input.IndexOf(" ") + 1;
+              // string str = input.Substring(i);
+              // Int32 sum = str.Split(new char[] { ',' })
+              //     .Select(n => Int32.Parse(n))
+              //     .Sum();
+              // Console.WriteLine("Result = {0}",sum);
+          }
+
+     }
+
+    
 }
